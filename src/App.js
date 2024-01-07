04b5afa6d41d94a25bbe1e1aed3e968a15109b5b
bk <kasparsbeikerts@gmail.com>
2024-01-07 18:57:29 +0000

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import "./App.css";
  * Uses Tailwind CSS for styling
  * Tailwind file is imported in App.css
  */
-
+<script 
+id="Cookiebot" 
+src="https://consent.cookiebot.com/uc.js" 
+data-cbid="efe52a23-4acb-4592-bcd8-a742469d3849" 
+data-blockingmode="auto" 
+type="text/javascript"
+></script>
 export default function App() {
   return (
     <div className="app min-h-screen text-blue-200 flex items-center flex-col p-20">
@@ -32,11 +38,27 @@ export default function App() {
       <div className="grid grid-cols-2 grid-rows-2 gap-4">
         <Button
           text="W"
-          url="https://www.streetcloth.com"
+          url="https://www.hyggefy.com"
         />
         <Button
           text="N"
-          url="https://streetcloth.com"
+          url="https://hyggefy.com"
+        />
+          <Button
+          text="N"
+          url="http://hyggefy.com"
+        />
+             <Button
+          text="N"
+          url="http://www.hyggefy.com"
+        />
+                  <Button
+          text="N"
+          url="www.hyggefy.com"
+        />
+          <Button
+          text="N"
+          url="hyggefy.com"
         />
       </div>
     </div>
